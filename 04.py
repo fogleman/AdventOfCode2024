4c1e90cda4aec53f5426f7e7a20a36aaf322d5e8
Michael Fogleman <fogleman@gmail.com>
2024-12-04 15:56:20 -0500

    cleanup

--- 04.py
+++ 04.py
@@ -1,53 +1,15 @@
-from collections import *
-from itertools import *
-from math import *
 import fileinput
 import re
 
-total = 0
-grid = []
-for line in fileinput.input():
-    line = line.rstrip()
-    grid.append(line)
+grid = [line.rstrip() for line in fileinput.input()]
+w, h = len(grid[0]), len(grid)
 
-w = len(grid[0])
-h = len(grid)
+get = lambda x, y: '.' if x < 0 or y < 0 or x >= w or y >= h else grid[y][x]
+gets = lambda x, y, d: ''.join(get(x + dx, y + dy) for dx, dy in d)
 
-def search(grid, x, y, path, deltas, pos, As):
-    if not all(q == deltas[0] for q in deltas):
-        return 0
-    word = ''.join(path)
-    if word == 'MAS':
-        # print(pos)
-        As[pos[-2]] += 1
-        return 1
-    if not 'MAS'.startswith(word):
-        return 0
-    if len(word) >= 3:
-        return 0
-    if x < 0 or y < 0 or x >= w or y >= h:
-        return 0
-    # print(path, pos, x, y)
-    result = 0
-    for dy in range(-1, 2):
-        for dx in range(-1, 2):
-            if dx == 0 or dy == 0:
-                continue
-            if dx == 0 and dy == 0:
-                continue
-            deltas.append((dx, dy))
-            path.append(grid[y][x])
-            pos.append((x, y))
-            result += search(grid, x + dx, y + dy, path, deltas, pos, As)
-            deltas.pop()
-            path.pop()
-            pos.pop()
-    return result
+print(sum(gets(x, y, [(dx * i, dy * i) for i in range(4)]) == 'XMAS'
+    for y in range(h) for x in range(w) for dy in [-1, 0, 1] for dx in [-1, 0, 1]))
 
-As = defaultdict(int)
-for y in range(h):
-    for x in range(w):
-        total += search(grid, x, y, [], [], [], As)
-
-print(total)
-print(sum(x == 2 for x in As.values()))
+print(sum(gets(x, y, [(-1, -1), (0, 0), (1, 1)]) in ['MAS', 'SAM'] and
+    gets(x, y, [(-1, 1), (0, 0), (1, -1)]) in ['MAS', 'SAM']
+        for y in range(h) for x in range(w)))

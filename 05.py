62fae31eb081cbd42d086313acf7b3366abed0b8
Michael Fogleman <fogleman@gmail.com>
2024-12-05 23:01:01 -0500

    cleanup

--- 05.py
+++ 05.py
@@ -1,18 +1,19 @@
 from functools import *
 import fileinput
-import re
 
-rules = []
+rules = {}
 updates = []
 for line in fileinput.input():
-    numbers = tuple(map(int, re.findall(r'\d+', line)))
     if '|' in line:
-        rules.append(numbers)
+        a, b = map(int, line.split('|'))
+        rules[(a, b)] = -1
+        rules[(b, a)] = 1
     elif ',' in line:
-        updates.append(numbers)
+        updates.append(tuple(map(int, line.split(','))))
 
-fix = lambda x: tuple(sorted(x, key=cmp_to_key(
-    lambda a, b: -1 if (a, b) in rules else 1 if (b, a) in rules else 0)))
+mid = lambda x: x[len(x) // 2]
+fix = lambda x: tuple(sorted(x,
+    key=cmp_to_key(lambda a, b: rules.get((a, b), 0))))
 
-print(sum(x[len(x) // 2] for x in updates if x == fix(x)))
-print(sum([fix(x)[len(x) // 2] for x in updates if x != fix(x)]))
+print(sum(mid(x) for x in updates if x == fix(x)))
+print(sum(mid(fix(x)) for x in updates if x != fix(x)))
